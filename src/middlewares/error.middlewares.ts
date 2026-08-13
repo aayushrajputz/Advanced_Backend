9e4570ce8ab270aa01eca15a6c4dac0ffb3e499d
aayushrajputz <aayushpratapsingh001@gmail.com>
2026-08-13 21:20:36 +0530

    auth api

---
+++ src/middlewares/error.middlewares.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from '../errors/app-errors.js'; // local dev me .ts/.js imports follow structural config
+import { ZodError } from 'zod';
+
+export const globalErrorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+): any => {
+    // If it is our custom operational error
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            success: false,
+            error: {
+                message: err.message,
+                statusCode: err.statusCode
+            }
+        });
+    }
+
+    if (err instanceof ZodError) {
+        return res.status(400).json({
+            success: false,
+            error: {
+                message: "Validation Error",
+                statusCode: 400,
+                details: err.issues.map((e) => ({
+                    field: e.path.join("."),
+                    message: e.message
+                }))
+            }
+        });
+    }
+
+    // If it is an unexpected system/programming bug (500)
+    console.error("🔥 SYSTEM ERROR:", err);
+    return res.status(500).json({
+        success: false,
+        error: {
+            message: "Something went wrong on our end",
+            statusCode: 500
+        }
+    });
+};
+
+
---
+++ src/middlewares/validate.middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { AnyZodObject } from "zod/v3";
+
+export const validateRequest = (schema: AnyZodObject) => {
+    return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            await schema.parseAsync({
+                body: req.body,
+                query: req.query,
+                params: req.params,
+            });
+
+            next();
+        } catch (error) {
+            next(error)
+        }
+    }
+} 
